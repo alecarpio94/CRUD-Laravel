c57d57c1a66e42a6a3eca706604318042dc9f5c7
Alejandro Carpio <alecarpio94@gmail.com>
2017-10-17 16:27:57 -0400

    mi primer commit

---
+++ app/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use App\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+	protected $table = 'posts';
+
+    protected $fillable = [
+		'title', 'body','user_id'
+	];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+}
